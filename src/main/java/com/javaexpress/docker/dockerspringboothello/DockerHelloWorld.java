c09bef27e45059886bf841fa3d1b5148a403b225
Venkata Bhaskar <66554216+javaexpresschannel@users.noreply.github.com>
2020-07-25 13:26:35 +0530

    Update DockerHelloWorld.java

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -11,6 +11,6 @@ public class DockerHelloWorld {
 	@GetMapping
 	public String getName() {
 		
-		return "Welcome to deployment in docker & new Deployment";
+		return "Welcome to deployment in docker ";
 	}
 }
